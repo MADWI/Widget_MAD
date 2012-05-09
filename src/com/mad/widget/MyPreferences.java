127b94df086c06de9fe4b5bf3582712bde99d5f4
Dawid Glinski <dawidg90@gmail.com>
2012-05-09 22:32:13 +0200

    Dodanie przycisku About

--- src/com/mad/widget/MyPreferences.java
+++ src/com/mad/widget/MyPreferences.java
@@ -1,10 +1,20 @@
 package com.mad.widget;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+//import android.app.AlertDialog;
+//import android.app.Dialog;
+//import android.content.DialogInterface;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+//import android.preference.Preference;
+//import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 
 public class MyPreferences extends PreferenceActivity {
@@ -23,6 +33,7 @@ public class MyPreferences extends PreferenceActivity {
 	private ListPreference listPreferenceYear;
 	private ListPreference listPreferenceGroup;
 	private CheckBoxPreference checkBoxPreference;
+	private Preference about_authors;
 	
 	
 	
@@ -36,9 +47,33 @@ public class MyPreferences extends PreferenceActivity {
 		listPreferenceYear = (ListPreference) findPreference("list_year");
 		listPreferenceGroup = (ListPreference)findPreference("list_group");
 		checkBoxPreference = (CheckBoxPreference) findPreference("checkbox");
+		about_authors = (Preference) findPreference("about");
+		about_authors.setOnPreferenceClickListener(about);
 
 		initPreferences();
 	}
+	
+	public OnPreferenceClickListener about = new OnPreferenceClickListener() {
+		
+		public boolean onPreferenceClick(Preference about_button) {
+			if(about_button.getTitle().equals("O nas"))
+				showDialog(0);
+			return false;
+		}
+	};
+	
+	protected Dialog onCreateDialog(int id)
+	{
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+		alert.setTitle("MADWI Widget").setMessage("Mobile Application Developers\nWydzia³ Informatyki ZUT \nKo³o naukowe\n::::::::::::::::\n\nAutorzy:\n:::::::::::::\nFabisiak Grzegorz\nGliñski Dawid\nPeryt Sebastian\nŒwierczek Sebastian\nWojtalewicz Micha³\n\nOpiekun ko³a:\n:::::::::::::::::::::::\ndr in¿. Rados³aw Maciaszczyk").setPositiveButton("Zamknij", new DialogInterface.OnClickListener() {
+			
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.cancel();
+			}
+		});
+		return alert.create();
+	}
+	
 
 	private void initPreferences() {
 	
